1e89dcdc10a2430b4c40df3b019d5950e07c0ba9
Danilo Bargen <mail@dbrgn.ch>
2014-09-04 17:06:08 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+from setuptools import setup, find_packages
+
+setup(
+    name='1337',
+    version='0.1.0',
+    description='This is so 1337!',
+    entry_points = {
+        '1337 = 1337:main'
+    }
+)
