559dcdb93bb3ae8dd5d9a6cd582a03a9535516b6
Danilo Bargen <mail@dbrgn.ch>
2014-09-04 17:11:36 +0200

    Changes

--- setup.py
+++ setup.py
@@ -5,9 +5,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='1337',
-    version='0.1.0',
+    version='1.0.0',
     description='This is so 1337!',
-    entry_points = {
-        '1337 = 1337:main'
+    long_description='Run\n\n::\n\n    $ 1337\n\n...to be 1337.',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            '1337 = 1337.1337:main',
+        ]
     }
 )
